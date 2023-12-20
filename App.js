fa7521365c44dd3bf5e4ae28e16d7dca30745468
şöhrett <sohretturaman@gmail.com>
2023-12-20 17:08:45 +0300

    expo psuh notification api has been used to send ,push notifications

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Platform, StyleSheet, Text, View } from "react-native";
 import * as Notifications from "expo-notifications";
 import * as Device from "expo-device";
 import { useEffect } from "react";
@@ -16,6 +16,36 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  useEffect(() => {
+    const getNotificationPermission = async () => {
+      const permissionState = await Notifications.getPermissionsAsync(); // gives current state of permission
+      console.log("permissionState", permissionState.status);
+      if (permissionState.status !== "granted") {
+        const askPersmission = await Notifications.requestPermissionsAsync(); //request permission
+        return askPersmission.status;
+      }
+      if (permissionState.status !== "granted") {
+        Alert.alert(
+          "Notification permission not granted",
+          "please proide notification permission"
+        );
+      }
+
+      const token = await Notifications.getExpoPushTokenAsync(
+        "5d0d322c-59d9-4477-823a-bb758a88ccb3"
+      );
+      console.log("token with expo id", token);
+      if (Platform.OS == "android") {
+        await Notifications.getNotificationChannelAsync("default", {
+          // should do that for andorid
+          name: "default",
+          importance: Notifications.AndroidImportance.HIGH,
+        });
+      }
+    };
+    getNotificationPermission();
+  }, []);
+
   useEffect(() => {
     const substriction = Notifications.addNotificationReceivedListener(
       (notification) => {
@@ -38,6 +68,20 @@ export default function App() {
       RemoveSubscription.remove();
     };
   }, []);
+
+  const SendPushNotificationHandler = () => {
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: "ExponentPushToken[Sy-jk9CIaM9JAK__1s5DhL]",
+        title: "you got the first notification ?",
+        body: "yes or no ?",
+      }),
+    });
+  };
 
   async function onSendLocalNotificaionHanler() {
     await Notifications.scheduleNotificationAsync({
@@ -62,6 +106,11 @@ export default function App() {
         title="send Local notification"
         onPress={onSendLocalNotificaionHanler}
       />
+      <Button
+        style={{ marginTop: 10 }}
+        title="send Push notification"
+        onPress={SendPushNotificationHandler}
+      />
     </View>
   );
 }
@@ -74,7 +123,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
+//Note : got that id ,after configure app.json file and eas build:configure from eas.json file
+//Can not initialize the getExpoPushTokenAsync() func without id in expo sdk 49
 //addNotificationRecivedListenet function returns a substriction object which  we can then  use for cleaning up that subsctiption ,so cleaning up and remove this event listener whenever we dont need it anymore. By return , useEffect will work whenever we got a new notification ,
 // whenever func is reexecuted or cleaned up
 //create a addNotificationResponseRecivedListender funtion for reacting incoming notifications
