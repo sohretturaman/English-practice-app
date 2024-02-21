7b14e650ef4d842c66f06f001ea056ebe5e87d86
şöhrett <sohretturaman@gmail.com>
2024-02-21 11:59:32 +0300

    got data from youtube search api response

--- src/components/youtube/SearchCard.js
+++ src/components/youtube/SearchCard.js
@@ -2,15 +2,56 @@
 
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image } from "react-native";
 
 const searchCard = () => {
   return (
-    <View>
-      <Text>searchCard</Text>
+    <View style={styles.containter}>
+      <View style={styles.imageWraper}>
+        <Image
+          source={require("../../contants/img/planeTravel.jpg")}
+          style={{ height: 100, width: "100%", resizeMode: "cover" }}
+        />
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.title} numberOfLines={2}>
+          Title is here : English practicig app with react native
+        </Text>
+        <Text style={styles.subtext}> channel name .....</Text>
+      </View>
     </View>
   );
 };
 
 export default searchCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containter: {
+    flexDirection: "row",
+    width: "100%",
+    height: 100,
+    padding: 5,
+  },
+  textContainer: {
+    padding: 5,
+    width: "55%",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    paddingBottom: 8,
+  },
+  imageWraper: {
+    width: "44%",
+  },
+  title: {
+    width: "100%",
+    lineHeight: 20,
+    paddingBottom: 5,
+    fontSize: 16,
+    color: "black",
+  },
+
+  subtext: {
+    fontSize: 12,
+  },
+});
--- src/screens/backup/SearchScreen.js
+++ src/screens/backup/SearchScreen.js
@@ -3,21 +3,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SearchBar from "../../components/uı/SearchBar";
-import Header from "../../components/drawer/Header";
 import { useNavigation } from "@react-navigation/native";
 import CustomHeader from "../../components/notes/CustomHeader";
+import SearchCard from "../../components/youtube/SearchCard";
 
 const SearchScreen = () => {
   const navigation = useNavigation();
   const getSearchData = async (searchInput) => {
     console.log("search input", searchInput);
-
+    //function and yotune api is working properly
     try {
       await fetch(
         `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCPJnjBeH7aJeq_4AnS-0E8SlG6cYBq-9M&q=${searchInput}&type=video&part=snippet&maxResults=2`
       )
         .then((res) => res.json())
-        .then((data) => console.log("data from api", data));
+        .then((data) => console.log("data yt from api", data));
     } catch (error) {
       console.log("an error accured while fetching data", error);
     }
@@ -27,6 +27,7 @@ const SearchScreen = () => {
     <View style={{ backgroundColor: "white", flex: 1 }}>
       <CustomHeader header={"Explore more on Youtube"} />
       <SearchBar placeholder="Search.." onSubmit={getSearchData} />
+      <SearchCard />
     </View>
   );
 };
